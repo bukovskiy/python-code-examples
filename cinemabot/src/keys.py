ac089f4457f0b93089adcc5897df984336ef9c97
bukovskiy <vlad.uspenskiy@gmail.com>
2020-02-10 15:42:00 +0300

    corrected minor typos

--- cinemabot/src/keys.py
+++ cinemabot/src/keys.py
@@ -2,7 +2,7 @@ import json
 
 # with open("./keys.json") as f:
 #     keys_dict = json.load(f)
-TG_API_TOKEN = "878059773:AAEDb_cYuQdIRH0R5IE_j3f-kJlON87TwYc"
+TG_API_TOKEN = TELEGRAM_KEY
 #TG_API_TOKEN = keys_dict["TELEGRAM_API_TOKEN"]
 
 
